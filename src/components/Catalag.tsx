90c7a9160c2cb0392580ffa9588c1c973600e1cb
Remulo Costa <remulo.costa@vulcanet.com.br>
2021-02-25 23:06:22 -0300

    Add catalog component

---
+++ src/components/Catalag.tsx
@@ -0,0 +1,24 @@
+/**
+ * IMPORTS
+ */
+import React from 'react'
+
+
+/**
+ * CODE
+ */
+
+ /**
+  *  I render catalog component
+  */
+ const Catalog: React.FC = () => {
+     return (
+         <h1>Catalog</h1>
+     )
+ }
+
+
+ /**
+  * EXPORTS
+  */
+ export default Catalog;
