ca730323f2f731f80fe0c57acd6c27b476044132
Remulo Costa <remulo.costa@vulcanet.com.br>
2021-02-25 22:56:25 -0300

    Initial redux configuration

---
+++ src/store/index.ts
@@ -0,0 +1,20 @@
+/**
+ * IMPORT
+ */
+import { createStore } from 'redux';
+
+
+/**
+ * CODE
+ */
+
+/**
+ *  I create store
+ */
+ const store = createStore(() => []);
+
+
+ /**
+  * EXPORT
+  */
+ export default store;
