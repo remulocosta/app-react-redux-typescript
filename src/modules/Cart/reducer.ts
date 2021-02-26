24b47ad51c9c5676398dadc65cf7a9bc6f90f10f
Remulo Costa <remulo.costa@vulcanet.com.br>
2021-02-26 00:08:03 -0300

    Initial state reducer cart

--- src/modules/Cart/reducer.ts
+++ src/modules/Cart/reducer.ts
@@ -1,12 +1,26 @@
 /**
+ * IMPORTS
+ */
+import { Reducer } from "redux";
+import { ICartState } from "./types";
+
+/**
+ * CONSTANTS AND DEFINITIONS
+ */
+const INITIAL_STATE: ICartState = {
+    items: []
+};
+
+
+/**
  * CODE
  */
 
 /**
  * I create cart reducer 
  */
-function cart() {
-    return [];
+const cart: Reducer<ICartState> = () => {
+    return INITIAL_STATE;
 }
 
 
---
+++ src/modules/Cart/types.ts
@@ -0,0 +1,17 @@
+/**
+ * TYPES
+ */
+export interface IProduct {
+    id: number;
+    title: string;
+    price: number;
+}
+
+export interface ICartItem {
+    product: IProduct;
+    quantity: number;
+}
+
+export interface ICartState {
+    items: ICartItem[];
+}
